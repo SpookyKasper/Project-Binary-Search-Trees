7f5912452783ce2f231f8f99d1a2284540eb41a1
Kasper <kasperbak89@pm.me>
2022-12-12 20:59:35 +0100

    Add a delete method to the Tree class that works for leafs

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -34,15 +34,6 @@ class Tree
     return Node.new(root, left_child, right_child)
   end
 
-  # algo insert:
-  # given a value
-  # if the value is smaller than the root
-  # recursively insert it in the left subtree
-  # if it is bigger than the root
-  # recursively insert it in the right subtree
-
-
-
   def insert(value, node=@root)
     if value < node.data
       node.left.nil? ? (node.left = Node.new(value) and return ) : left_sub_tree = node.left
@@ -50,6 +41,30 @@ class Tree
     else
       node.right.nil? ? (node.right = Node.new(value) and return) : right_sub_tree = node.right
       insert(value, right_sub_tree)
+    end
+  end
+
+  # Pseudo code for delete method
+  # start with deleting leafs
+  # Given a value
+  # Look for that value in the tree as following
+  # start at the root, if the value is smaller than the root.data
+    # recursively call the delete method on the left sub tree
+    # else recursively call delete on the right sub tree
+  # Base case, if next.left or next.right == the value
+  # next.left or next.right gets nil instead
+
+  def delete(value, node=@root)
+    # if node.left.data == value || node.right.data == value
+    #   node.left == value ? node.left = nil : node.right = nil
+    #   return
+    # end
+    if value < node.data
+      p 'goes left'
+      node.left.data == value ? (node.left = nil and return) : delete(value, node.left)
+    else
+      p 'goes right'
+      node.right.data == value ? (node.right = nil and return) : delete(value, node.right)
     end
   end
 end
@@ -64,14 +79,13 @@ my_simple_tree = Tree.new(simple_array)
 my_complicated_tree = Tree.new(clean_data)
 
 
-my_complicated_tree.pretty_print
-my_complicated_tree.insert(24)
-my_complicated_tree.insert(78)
-my_complicated_tree.insert(350)
-my_complicated_tree.insert(10)
-my_complicated_tree.pretty_print
+# my_complicated_tree.pretty_print
+# my_complicated_tree.insert(24)
+# my_complicated_tree.pretty_print
 
 my_simple_tree.pretty_print
 my_simple_tree.insert(8)
-my_simple_tree.insert(9)
+my_simple_tree.pretty_print
+
+my_simple_tree.delete(3)
 my_simple_tree.pretty_print
