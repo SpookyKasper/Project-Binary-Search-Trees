2923050b5d9fb9b95552697f2a900bb63cac0a42
Kasper <kasperbak89@pm.me>
2023-01-08 00:06:57 +0100

    Clean a bit the code

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -3,7 +3,7 @@ class Node
 
   attr_accessor :left, :data, :right
 
-  def initialize(data, left=nil, right=nil)
+  def initialize(data, left = nil, right = nil)
     @data = data
     @left = left
     @right = right
@@ -11,20 +11,15 @@ class Node
 
   def count_children
     count = 0
-    unless @left.nil? then count += 1 end
-    unless @right.nil? then count += 1 end
+    count += 1 unless @left.nil?
+    count += 1 unless @right.nil?
     count
-  end
-end
-
-module Comparable
-  def compare(node1, node2)
-    node1.data == node2.data
   end
 end
 
 class Tree
   attr_reader :root
+
   def initialize(array)
     @root = build_tree(array)
   end
@@ -36,14 +31,14 @@ class Tree
   end
 
   def build_tree(array)
-    return nil if array.empty?
+    return if array.empty?
     return Node.new(array[0]) if array.length < 2
 
     array = array.uniq.sort
-    root_index = array.length/2
+    root_index = array.length / 2
     root = array[root_index]
-    left = array[0..root_index-1]
-    right = array[root_index+1..]
+    left = array[0..root_index - 1]
+    right = array[root_index + 1..]
     left_child = build_tree(left)
     right_child = build_tree(right)
     Node.new(root, left_child, right_child)
@@ -68,7 +63,7 @@ class Tree
 
   def delete(value, root = @root, mama = @root)
     return if root.nil?
-    if value == root.data then delete_node(root, mama) and return value end
+    delete_node(root, mama) and return value if value == root.data
 
     value < root.data ? delete(value, root.left, root) : delete(value, root.right, root)
   end
@@ -99,7 +94,7 @@ class Tree
       current_node = discovered_nodes[0]
       discovered_nodes << current_node.left unless current_node.left.nil?
       discovered_nodes << current_node.right unless current_node.right.nil?
-      block ? result << (yield current_node) : result << current_node.data
+      result << (block ? (yield current_node) : current_node.data)
       discovered_nodes.shift
     end
     result
@@ -111,7 +106,7 @@ class Tree
     current_node = discovered_nodes[0]
     discovered_nodes << current_node.left unless current_node.left.nil?
     discovered_nodes << current_node.right unless current_node.right.nil?
-    block ? result << (yield current_node) : result << current_node.data
+    result << (block ? (yield current_node) : current_node.data)
     discovered_nodes.shift
     level_order_rec(result, discovered_nodes, &block)
   end
@@ -144,6 +139,7 @@ class Tree
   end
 
   def depth(node, current_node = @root)
+    return if node.nil?
     return 0 if node.data == current_node.data
 
     if node.data < current_node.data
@@ -155,59 +151,49 @@ class Tree
   end
 
   def height(node)
-    return nil if node.nil?
+    return if node.nil?
     return 0 if node.left.nil? && node.right.nil?
 
-    node.left ? path_left = 1 + height(node.left) : path_left = 0
-    node.right ? path_right = 1 + height(node.right) : path_right = 0
-    height = path_left >= path_right ? path_left : path_right
-    height
+    path_left = node.left ? 1 + height(node.left) : 0
+    path_right = node.right ? 1 + height(node.right) : 0
+    path_left >= path_right ? path_left : path_right
   end
-
-  # pseudocode for balanced?
-  # substract the height of the left subtree to the height of the right subtree
-  # if the absolut value of the result is bigger than 2 return false
-  # else recursively call the balanced? on the left subtree
-  # recursively call the balanced? on the right subtree
-  # return true
 
   def balanced?(current_node = @root)
     return true if current_node.nil?
     return true if current_node.left.nil? && current_node.right.nil?
+
     left_subtree_height = height(current_node.left) || -1
     right_subtree_height = height(current_node.right) || -1
     diff = left_subtree_height - right_subtree_height
     return false if diff.abs > 1
+
     result_left = balanced?(current_node.left)
     return false unless result_left
+
     result_right = balanced?(current_node.right)
     return false unless result_right
+
     true
   end
 
-  # pseudo for rebalance method
-  # if the tree is balanced return
-  # oterwhise, make an array of the nodes with a traversal method
-  # self gets the result of calling build tree on that array
-
   def rebalance
-    return if self.balanced?
+    return if balanced?
 
-    array = self.inorder
+    array = inorder
     @root = build_tree(array)
   end
-
 end
 
 easy = [1, 2, 3, 5]
 medium = [1, 2, 3, 4, 5, 6, 7, 8]
-hardcore = [1, 7, 4, 24, 23, 10, 12, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 3234, 12338, 12, 234, 29, 98, 200, 245, 11]
+hardcore = [1, 7, 4, 24, 23, 10, 12, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 3234, 12_338, 12, 234, 29, 98, 200, 245, 11]
 
 easy_tree = Tree.new(easy)
 medium_tree = Tree.new(medium)
 hardcore_tree = Tree.new(hardcore)
 
-new_binary_search_tree = Tree.new(Array.new(20) {rand(1..100)})
+new_binary_search_tree = Tree.new(Array.new(20) { rand(1..100) })
 new_binary_search_tree.pretty_print
 p new_binary_search_tree.balanced?
 p new_binary_search_tree.level_order_rec
@@ -226,4 +212,3 @@ p new_binary_search_tree.level_order_rec
 p new_binary_search_tree.preorder
 p new_binary_search_tree.inorder
 p new_binary_search_tree.postorder
-
